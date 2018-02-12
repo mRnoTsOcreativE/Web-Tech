9a8c18775f468caba2b25cdaabadb204b90b4506
Md. Borhan <aiub15297972@gmail.com>
2018-02-12 13:55:13 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,40 +1,46 @@
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-	<title>Home</title>
-	<meta charset="utf-8">
-	<style>
-		h1 
-		{
-			background-color: #43bc8c; 
-			color: red;
-			text-align: center;
-			font-size: 50px
+<?php
+	echo "Hello<br>";
+	$a=5;
+	$b=10;
+	$i=0;
+	$s=5;
+	$k=5;
+	echo $a+$b ."<br>";
+	for($i; $i<100; $i++){
+		echo  ($i+1) . " Line " .($i+1). "<br>";
+	}
+	$cgpa=array("A"=>3,"B"=>3.4,"C"=>3.5);
+	echo "Cgpa of student B :". $cgpa['B'] . "<br>";
+	foreach($cgpa as $name=>$value){
+		echo "Name : ". $name ." Cgpa : ". $value ."<br>";
+	}
+	echo "<br>";
+	for($m=0;$m<5;$m++){
+		for($n=0;$n<=$m;$n++){
+			echo "*";
 		}
-		img
-		{
-			width: 90px;
-			height: 90px;
-			padding-right: 70px;
-			padding-left: 20px;
+		echo "<br>";
+	}
+	
+	for($m=0;$m<5;$m++){
+		for($n=5;$n>$m;$n--){
+			echo "*";
 		}
-	</style>
-</head>
-<body>
-	<h1>Student Management System</h1>
-	<hr>
-	<div style="text-align: center;">
-		<img src="img/home.ico" alt="home">
-		<a href="info.php"><img src="img/info.png" alt="info"></a>
-		<a href="registration.php"><img src="img/register.png" alt="Registration"></a>
-	</div>
-	<div>
-		<h3>Description</h3>
-		<p>(For most people) a Student management system (SMS) is software to manage all day to day
-		operations for a school. They are also called student information systems (SIS), Student
-		information management systems (SIMS), Student records system (SRS). Functionalities of such
-		systems are not the same and this may reflect in the name adopted by users and vendors.Some
-		learning management systems also may provide SMS functionality, but the two are different.</p>
-	</div>
-</body>
-</html>
+		echo "<br>";
+	}
+	
+	for($m=1;$m<=5;$m++){
+		for($n=5;$n>=1;$n--){
+			if($y<=$x)
+			{
+				echo "*";
+			}
+			else
+				echo "$nbsp";
+			
+		}
+		for($n=0)
+		echo "<br>";
+	}
+	
+?>
