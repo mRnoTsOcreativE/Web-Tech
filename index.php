d60dadcfd470e60c6dea8fa41840c99d9e0c0af6
Md. Borhan <aiub15297972@gmail.com>
2018-02-12 13:54:35 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,40 @@
-<!doctype>
-<html>
-	<head>
-		<title>
-			form.html
-		</title>
-	</head>
-	
-	<body>
-		<h1 align="center">Student Management System</h1>
-		<img src="car.png" alt="image" width="80" height="80"/>		
-		<form action="StudentInfo.php">
-			<input type="Submit" value="StudentInfo"/>
-		<form action="RegistrationForm.php">
-			<input type="Submit" value="RegistrationForm"/>
-	</body>
-		
-	
-	
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>Home</title>
+	<meta charset="utf-8">
+	<style>
+		h1 
+		{
+			background-color: #43bc8c; 
+			color: red;
+			text-align: center;
+			font-size: 50px
+		}
+		img
+		{
+			width: 90px;
+			height: 90px;
+			padding-right: 70px;
+			padding-left: 20px;
+		}
+	</style>
+</head>
+<body>
+	<h1>Student Management System</h1>
+	<hr>
+	<div style="text-align: center;">
+		<img src="img/home.ico" alt="home">
+		<a href="info.php"><img src="img/info.png" alt="info"></a>
+		<a href="registration.php"><img src="img/register.png" alt="Registration"></a>
+	</div>
+	<div>
+		<h3>Description</h3>
+		<p>(For most people) a Student management system (SMS) is software to manage all day to day
+		operations for a school. They are also called student information systems (SIS), Student
+		information management systems (SIMS), Student records system (SRS). Functionalities of such
+		systems are not the same and this may reflect in the name adopted by users and vendors.Some
+		learning management systems also may provide SMS functionality, but the two are different.</p>
+	</div>
+</body>
 </html>
