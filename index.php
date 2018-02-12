a441c75fb5eecbaad7918e288d1edb33a126630b
Md. Borhan <aiub15297972@gmail.com>
2018-02-12 13:53:47 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype>
+<html>
+	<head>
+		<title>
+			form.html
+		</title>
+	</head>
+	
+	<body>
+		<h1 align="center">Student Management System</h1>
+		<img src="car.png" alt="image" width="80" height="80"/>		
+		<form action="StudentInfo.php">
+			<input type="Submit" value="StudentInfo"/>
+		<form action="RegistrationForm.php">
+			<input type="Submit" value="RegistrationForm"/>
+	</body>
+		
+	
+	
+</html>
